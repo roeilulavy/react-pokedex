6d1a0f137165be7d9c58d77d1e27f1eaebafeff9
Roy Lulavy <49700286+roeilulavy@users.noreply.github.com>
2022-03-27 13:47:57 +0300

    build start 2

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import api from "../utils/api";
+import '../index.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
