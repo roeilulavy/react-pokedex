a75e7cba0bd65c826e99daa7f9e7012bff5db85f
Roy Lulavy <49700286+roeilulavy@users.noreply.github.com>
2022-03-27 16:16:04 +0300

    top cover

--- src/components/TopCover.js
+++ src/components/TopCover.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import Prev from "../images/icon/undo.png";
+import Search from "../images/icon/search.svg";
+import Next from "../images/icon/redo.png";
 
 const TopCover = () => {
   return (
     <div className='top-cover'>
-      <div className='top-cover__buttons'>
-        <button></button>
-        <button></button>
+      <div className='top-cover__container'>
+        <img className='top-cover__button' src={Prev} alt='Prev'/>
+        <img className='top-cover__button' src={Search} alt='Search'/>
+        <img className='top-cover__button' src={Next} alt='Next'/>
       </div>
-      <button></button>
     </div>
   )
 }
