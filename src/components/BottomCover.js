e6bfa130aef280a982da4e437b658ea20217a95a
Roy Lulavy <49700286+roeilulavy@users.noreply.github.com>
2022-03-27 14:44:14 +0300

    Layout build

---
+++ src/components/BottomCover.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const BottomCover = () => {
+  return (
+    <div className='bottom-cover'>
+      <div className='bottom-cover__buttons'>
+        <button></button>
+        <button></button>
+      </div>
+      <button></button>
+    </div>
+  )
+}
+
+export default BottomCover;
---
+++ src/components/TopCover.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TopCover = () => {
+  return (
+    <div className='top-cover'>
+      <div className='top-cover__buttons'>
+        <button></button>
+        <button></button>
+      </div>
+      <button></button>
+    </div>
+  )
+}
+
+export default TopCover;
