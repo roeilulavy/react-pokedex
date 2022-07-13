1b66c4eae3dc77d68aa9650ecc9a2f7a8ea73c3e
Roy Lulavy <49700286+roeilulavy@users.noreply.github.com>
2022-07-13 12:05:44 +0300

    Ass Loader

--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -1,9 +1,17 @@
+import { useState } from 'react';
 import './PokemonList.css';
+import Pokeload from '../../images/Pokeball-gif.gif';
 
 export default function PokemonList() {
+
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <div className='PokemonList'>
+      {isLoading &&
+        <img className='PokemonList__loader' src={Pokeload} alt='Loader'/>
       
+      }
     </div>
   );
 }
