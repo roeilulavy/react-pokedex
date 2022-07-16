6f4e3a1a1bc5e94dbfdfc5dfc2af3fa61d753c89
Roy Lulavy <49700286+roeilulavy@users.noreply.github.com>
2022-07-16 16:26:37 +0300

    Add Container

---
+++ src/components/PokemonListItem/PokemonListItem.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import './PokemonListItem.css';
+import Pokeload from '../../images/pokeball-jump.gif';
+
+export default function PokemonItem({ item, id, image, name, setPokemonInfo, setIsInfoOpen }) {
+
+  const [isImageLoading, setIsImageLoading] = useState(false);
+
+  function onPokemonClick(pokemonData) {
+    setPokemonInfo(pokemonData);
+    setIsInfoOpen(true);
+  }
+
+  useEffect(() => {
+    if (image) {
+      setIsImageLoading(false)
+    }
+  }, [image]);
+
+  return(
+    <div className='PokemonItem' key={id} onClick={() => onPokemonClick(item)}>
+      {isImageLoading ? 
+        <img className='PokemonItem__image' src={Pokeload} alt='Loading pokemon'/>
+      :
+        <>
+          <p className='PokemonItem__id' >{id}</p>
+          <img className='PokemonItem__image' src={image} alt={name}/>
+          <p className='PokemonItem__name'>{name}</p>
+        </>
+      }
+      
+    </div>
+  );
+}
