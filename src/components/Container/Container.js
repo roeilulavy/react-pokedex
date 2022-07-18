f1094f86985a46a86f146243b0513a1d5f0f3acb
Roy Lulavy <49700286+roeilulavy@users.noreply.github.com>
2022-07-18 16:26:58 +0300

    Fix Search

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -2,34 +2,14 @@ import './Container.css';
 import Pokeload from '../../images/Pokeball-gif.gif';
 import PokemonList from '../PokemonList/PokemonList';
 import PokemonInfoCard from '../PokemonInfoCard/PokemonInfoCard';
-import CloseBtn from '../../images/icon/close.png';
 
-export default function Container({ url, isLoading, pokemonList, pokemonId, setPokemonId, isInfoOpen, setIsInfoOpen, searchInputOpen, searchKeyword, setSearchKeyword }) {
-
-  const clearText = () => {
-    setSearchKeyword('');
-  }
-
+export default function Container({ url, isLoading, pokemonList, pokemonId, setPokemonId, isInfoOpen, setIsInfoOpen, searchKeyword }) {
   return(
     <div className='Container'>
       {isLoading ?
         <img className='PokemonList__loader' src={Pokeload} alt='Loader'/>
       :
         <>
-          {searchInputOpen &&
-            <div className={`${isInfoOpen ? 'Container__search-container_close' : 'Container__search-container'}`}>
-              <input
-                className='Container__search-input'
-                type='text'
-                placeholder='Enter Name or ID'
-                value={searchKeyword}
-                onChange={e => setSearchKeyword(e.target.value)}
-              />
-              <button className='Container__search-clear' onClick={() => clearText()}>
-                <img className='Container__search-clear-icon' src={CloseBtn} alt='Close button'/>
-              </button>
-            </div>
-          }
           <PokemonList
             pokemonList={pokemonList}
             setPokemonId={setPokemonId}
--- src/components/CoverTop/CoverTop.js
+++ src/components/CoverTop/CoverTop.js
@@ -3,8 +3,9 @@ import './CoverTop.css';
 import PrevBtn from '../../images/icon/undo.png';
 import NextBtn from '../../images/icon/redo.png';
 import SearchBtn from '../../images/icon/search.svg';
+import CloseBtn from '../../images/icon/close.png';
 
-export default function CoverTop({ isOpen, isInfoOpen, searchInputOpen, setSearchInputOpen, onNextPokemonClick, onPrevPokemonClick }) {
+export default function CoverTop({ isOpen, isInfoOpen, searchInputOpen, setSearchInputOpen, onNextPokemonClick, onPrevPokemonClick, searchKeyword, setSearchKeyword }) {
 
   const [showContent, setShowContent] = useState(false);
 
@@ -33,6 +34,10 @@ export default function CoverTop({ isOpen, isInfoOpen, searchInputOpen, setSearc
     onPrevPokemonClick()
   }
 
+  const clearText = () => {
+    setSearchKeyword('');
+  }
+
   return (
     <div className={`CoverTop ${isOpen && 'CoverTop_open'}`}>
       {showContent && 
@@ -50,6 +55,21 @@ export default function CoverTop({ isOpen, isInfoOpen, searchInputOpen, setSearc
         </div>
       }
       <div className='CoverTop__background' />
+
+      {searchInputOpen &&
+        <div className={`${isInfoOpen ? 'CoverTop__search-container_close' : 'CoverTop__search-container'}`}>
+          <input
+            className='CoverTop__search-input'
+            type='text'
+            placeholder='Enter Name or ID'
+            value={searchKeyword}
+            onChange={e => setSearchKeyword(e.target.value)}
+          />
+          <button className='CoverTop__search-clear-button' onClick={() => clearText()}>
+            <img className='CoverTop__search-clear-button-icon' src={CloseBtn} alt='Close button'/>
+          </button>
+        </div>
+      }
     </div>
   );
 }
